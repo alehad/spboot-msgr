4a52709f06d4198d0019309e8be884079d997364
alehad <64356216+alehad@users.noreply.github.com>
2022-06-12 12:10:32 +0100

    SpringBoot app tidy up
    resolved issues with @Autowired in RestController

--- src/main/java/msgr/app/MessengerAppController.java
+++ src/main/java/msgr/app/MessengerAppController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import msgr.db.IMessageStore;
-//import msgr.db.SimpleDbStore;
 import msgr.msg.Message;
 
 import msgr.svc.MessageStoreService;
@@ -18,18 +16,17 @@ import msgr.svc.MessageStoreService;
 @RestController
 public class MessengerAppController {
 	
-//	@Autowired
-//	private MessageStoreService messageStoreService;
-
-	private MessageStoreService messageStoreService = new MessageStoreService();
-
-//	@Autowired
-//	private IMessageStore messageStore = new SimpleDbStore();
-	private IMessageStore messageStore;
-	
-	public MessengerAppController() {
-		messageStore = messageStoreService.getStore();
-	}
+	/*
+	 * **IMPORTANT**
+	 * Auto-wiring only happens AFTER MessengerAppController construction
+	 * which means that you cannot:
+	 * 1. access the instance of the message store service in another variable inline initiation, or 
+	 * 2. in the MessengerAppController constructor itself
+	 * In both of these cases, you will get null pointer exception as the msg store service would not be 
+	 * initialised at that point 
+	 */
+	@Autowired
+	private MessageStoreService messageStoreService;
 
 	// for GET methods, you only need to provide URL path
 	@RequestMapping("/welcome")
@@ -39,26 +36,26 @@ public class MessengerAppController {
 
 	@RequestMapping("/messages")
 	public List<Message> getMessages() {
-		return messageStore.getMessages();
+		return messageStoreService.getStore().getMessages();
 	}
 
 	@RequestMapping("/messages/by/{byAuthor}")
 	public List<Message> getMessagesBy(@PathVariable String byAuthor) {
-		return messageStore.getMessagesBy(byAuthor);
+		return messageStoreService.getStore().getMessagesBy(byAuthor);
 	}
 
 	@RequestMapping(method=RequestMethod.POST, value="/messages")
 	public Message postMessage(@RequestBody Message msg) {
-		return messageStore.createMessage(msg);
+		return messageStoreService.getStore().createMessage(msg);
 	}
 
 	@RequestMapping(method=RequestMethod.PUT, value="/messages/by/{byAuthor}")
 	public Message updateMessageBy(@RequestBody Message newMsg, @PathVariable String byAuthor) {
-		return messageStore.updateMessageBy(byAuthor, newMsg);
+		return messageStoreService.getStore().updateMessageBy(byAuthor, newMsg);
 	}
 
 	@RequestMapping(method=RequestMethod.DELETE, value="/messages/by/{byAuthor}")
 	public boolean deleteMessagesBy(@PathVariable String byAuthor) {
-		return messageStore.deleteMessagesBy(byAuthor);
+		return messageStoreService.getStore().deleteMessagesBy(byAuthor);
 	}
 }
--- src/main/java/msgr/app/MessengerAppServer.java
+++ src/main/java/msgr/app/MessengerAppServer.java
@@ -4,9 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+/*
+ * ** IMPORTANT **
+ * Spring Boot will only scan:
+ * a. the package where the class annotated with @SpringBootApplication is located, and
+ * b. all the sub packages beneath that one
+ * if the app has flat package structure than you need to specify component scan path
+ * but in that case you also need to include the package where the rest controller is found
+ * otherwise you will get 404 error, as spring will not be able to wire the rest controller 
+ */
 @SpringBootApplication
 @ComponentScan(basePackages = {"msgr.svc", "msgr.app"})
-//@ComponentScan({"msgr.db"})
 public class MessengerAppServer {
 
 	public static void main(String[] args) {
