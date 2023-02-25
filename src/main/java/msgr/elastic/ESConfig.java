5dfeaa3cc8e6c88ebad133a5db4409fdfa3c8b88
alehad <64356216+alehad@users.noreply.github.com>
2023-02-25 19:02:51 +0000

    Update ESConfig.java
    changes to ES connection string to see if github maven build will succeed.

--- src/main/java/msgr/elastic/ESConfig.java
+++ src/main/java/msgr/elastic/ESConfig.java
@@ -8,6 +8,7 @@ import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.http.HttpHeaders;
 
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "msgr.elastic")
@@ -22,9 +23,27 @@ public class ESConfig extends AbstractElasticsearchConfiguration {
 	
 	@Override
 	public RestHighLevelClient elasticsearchClient() {
+		/*
         ClientConfiguration clientConfiguration = ClientConfiguration.builder().connectedTo(_host + ":" + _port).build();
 
         return RestClients.create(clientConfiguration).rest();
+        */
+		//Below 3 lines are added to make elastic client version 7.17.4 compatible with elastic server version 8x
+		 HttpHeaders compatibilityHeaders = new HttpHeaders();
+		        compatibilityHeaders.add("Accept", "application/vnd.elasticsearch+json;compatible-with=7");
+		        compatibilityHeaders.add("Content-Type", "application/vnd.elasticsearch+json;"
+		                + "compatible-with=7");
+
+		        final ClientConfiguration clientConfiguration =
+		                ClientConfiguration
+		                        .builder()
+		                        .connectedTo(_host + ":" + _port)
+		      //                  .usingSsl()
+		      //                  .withBasicAuth(esConnectionDetails.getUsername(), esConnectionDetails.getPassword())
+		                        .withDefaultHeaders(compatibilityHeaders)    // this variant for imperative code
+		                        .build();
+
+		return RestClients.create(clientConfiguration).rest();
     }
 
 }
