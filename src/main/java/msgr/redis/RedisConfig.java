599cf01b93134ceff7f1983b5f5c231ed4f89ba0
alehad <64356216+alehad@users.noreply.github.com>
2023-06-18 09:47:42 +0100

    fixed msgr-docker-compose and redis config
    so that redis can be accessed from the msgr docker app. also added depends on for both redis and ES condition in yml.

--- src/main/java/msgr/redis/RedisConfig.java
+++ src/main/java/msgr/redis/RedisConfig.java
@@ -1,31 +1,34 @@
 package msgr.redis;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-import org.springframework.data.redis.serializer.GenericToStringSerializer;
 
 @Configuration
 @ComponentScan("msgr.app")
 @EnableRedisRepositories(basePackages = "msgr.redis")
-@PropertySource("classpath:application.properties")
 public class RedisConfig {
+    @Value("${spring.redis.host}")
+    private String REDIS_HOST;
+    @Value("${spring.redis.port}")
+    private Integer REDIS_PORT;
 
     @Bean
-    JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
+    public JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(REDIS_HOST, REDIS_PORT);
+        return new JedisConnectionFactory(config);
     }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
-        final RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setEnableTransactionSupport(true);
         template.setConnectionFactory(jedisConnectionFactory());
-        template.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
         return template;
     }
-
 }
